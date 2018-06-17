da7289c68aba98ef427e5f36d49cd4c55412de02
Yuske Fukuyama <yuskefuku@gmail.com>
2018-06-17 21:29:45 +0900

    try audio message type

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-# ACCESS_TOKEN = 'EayIwmR9wSGAd2d+c49vjErYHI+myz06mwqhSqdm46RrEehI1NHJ6PvvSBJJdBFIxZtjexNCSVBVLl9Px/jznYaMcpv2ZWwV4BtHWC6mhbjeX+v0KlcfWcRV8fgkuszWcNOlp52rDpu2OsvvtO/ZewdB04t89/1O/w1cDnyilFU='
-#
-# CHANNEL_SECRET = '9862032ed68823de2b01b02b6e2ca998'
-channel_access_token = os.environ.get("LINE_ACCESS_TOKEN", None)
-channel_secret = os.environ.get("LINE_CHANNEL_SECRET", None)
+# channel_access_token = os.environ.get("LINE_ACCESS_TOKEN", None)
+# channel_secret = os.environ.get("LINE_CHANNEL_SECRET", None)
+
+channel_access_token = os.getenv("LINE_ACCESS_TOKEN", None)
+channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
 
 if channel_secret is None:
     print('env_variable: LINE_CHANNEL_SECRET is not set')
@@ -35,7 +35,7 @@ if channel_access_token is None:
     print('env_variable: LINE_ACCESS_TOKEN is not set')
 
 line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secrete)
+handler = WebhookHandler(channel_secret)
 
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
@@ -55,6 +55,7 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+
 @app.route('/callback', methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -70,12 +71,38 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if event.message.text == 'ping':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='pong')
+        )
+
+
+@handler.add(MessageEvent, message=AudioMessage)
+def handle_content_message(event):
+    if isinstance(event.message, AudioMessage):
+        extension = 'm4a'
+    else:
+        return
+
+    content = line_bot_api.get_message_content(event.message.id)
+    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=extension+'-', delete=False) as tf:
+        for chunk in content.iter_content():
+            tf.write(chunk)
+        tempfile_path = tf.name
+
+    dist_path = tempfile_path + '.' + extension
+    dist_name = os.path.basename(dist_path)
+    os.rename(tempfile_path, dist_path)
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))
     )
+
 
 @app.errorhandler(500)
 def server_error(e):
@@ -87,6 +114,15 @@ def server_error(e):
 
 
 if __name__ == '__main__':
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=8080, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    make_static_tmp_dir()
+
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
